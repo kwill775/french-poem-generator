6dfb8618ccad9985621f0435311678754103a819
kwill775 <spn775@yahoo.com>
2024-12-22 22:22:05 -0700

    improved the user experience

--- src/index.js
+++ src/index.js
@@ -12,10 +12,14 @@ function generatePoem(event) {
 
   let topicInput = document.querySelector("#user-topic");
   let apiKey = "3tb400c941f2a4edo23f9347ffbbacfd";
-  let prompt = `User topic: Genetate a French poem about ${topicInput}`;
+  let prompt = `Topic: Genetate a French poem about ${topicInput}`;
   let context =
-    "You have an extensive knowledge of poems and love to write short poems. Your mission is to generate a 4 line poem and separate each line with <br />. Make sure to follow the user topic.";
+    "You have an extensive knowledge of poems and love to write short poems. Please use the topic provided to you to generate a poem. Your mission is to generate a 4 line poem and separate each line with <br />. ";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<span class="blink">⌛</span>Generating a French poem about ${topicInput.value}...`;
 
   axios.get(apiUrl).then(displayPoem);
 }
